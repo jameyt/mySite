16608c3c4ab831f49dbf169a78ccb78f4362894a
Jamey Tyler <jameyerictyler@gmail.com>
2016-02-10 15:47:47 -0600

    flatly theme with static client side files

--- index.js
+++ index.js
@@ -16,22 +16,61 @@
 // // Put a friendly message on the terminal
 // console.log("Server running at http://127.0.0.1:8000/");
 
-var express = require('express');
+//---------------------------------------------------------------------------------
+
+// var express = require('express');
+// var app = express();
+// 
+// app.set('port', (process.env.PORT || 5000));
+// 
+// //app.use(express.static(__dirname + '/public'));
+// 
+// // views is directory for all template files
+// //app.set('views', __dirname + '/views');
+// //app.set('view engine', 'ejs');
+// 
+// app.get('/', function(request, response) {
+//     response.send('Hello World!');
+//   //response.render('pages/index');
+// });
+// 
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+//---------------------------------------------------------------------------------
+
+
+var express = require("express");
 var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
 
-app.set('port', (process.env.PORT || 5000));
+app.use(express.static('public'));
 
-//app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-    response.send('Hello World!');
-  //response.render('pages/index');
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+router.get("/",function(req,res){    
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
+
+// app.use("*",function(req,res){
+//   res.sendFile(path + "index.html");  
+// });
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
 });
